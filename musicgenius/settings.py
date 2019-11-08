bdbd4368411012530a7d3f29037315e8ac1c36d1
LEARN Academy <learnacademy@LEARNs-MacBook-Pro.local>
2019-11-07 16:34:12 -0800

    auth users with token

--- musicgenius/settings.py
+++ musicgenius/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%t*jd6latx2@z6#t(&h8_=0(!pf2o14138aqo5hs+!-vqail*)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,15 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
-    'app.apps.MusicgeniusConfig',
+    'api'
+    # 'app.apps.MusicgeniusConfig',
     # 'musicgenius.app.AppConfig'
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -71,10 +73,10 @@ MIDDLEWARE = [
 #     ]
 # }
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 10
+# }
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
